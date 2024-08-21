e3b5d672971a535d182ec8fb337e8a46dc3b395a
Wra2007 <lauren.quinones@correounivalle.edu.co>
2024-08-21 10:31:27 -0500

    Add files via upload

---
+++ Labo12SM.py
@@ -0,0 +1,9 @@
+class Transporte:
+    cilindraje="125"
+    modelo=2024
+    color="Negro"
+
+motocicleta = Transporte()
+print(motocicleta.cilindraje)
+print(motocicleta.modelo)
+print(motocicleta.color)
